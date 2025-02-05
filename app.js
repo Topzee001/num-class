037771440f367ddc54d50d6710ba24ff54adca0a
Ibrahim <ibrahimsakariyah@gmail.com>
2025-02-05 15:12:04 +0100

    vercel end

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.use(express.json());
 
 //route
 app.use("/", numberRouter);
-app.use("/api/classify-number", numberRouter);
+// app.use("/api/classify-number", numberRouter);
+app.use("/classify-number", numberRouter);
 
 module.exports = app;
