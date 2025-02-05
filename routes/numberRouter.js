c44c965bda3055095679bbf065008a33add39fb0
Ibrahim <ibrahimsakariyah@gmail.com>
2025-02-05 15:38:00 +0100

    verc

--- routes/numberRouter.js
+++ routes/numberRouter.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 router.route("/").get(numberController.getWelcome);
 
-router.route("/api/classify-number").get(numberController.getWelcome);
-// router.route("/api/classify-number").get(numberController.getNumberClass);
+router.route("/api/classify-number").get(numberController.getNumberClass);
 
 module.exports = router;
